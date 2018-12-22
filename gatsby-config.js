1b8df153b66a0c4b0c623b35ea61a88be3b245e9
Ryo Watanabe <ryo.watanabe@gmail.com>
2018-12-22 15:29:07 +0900

    Add gatsby-plugin-feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,65 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: (post) => {
+              const rssMetadata = post.query.site.siteMetadata
+              return post.query.allAirtable.edges.map(edge => ({
+                date: edge.node.data.date,
+                title: edge.node.data.title,
+                description: edge.node.data.body.childMarkdownRemark.excerpt,
+                url: rssMetadata.siteUrl + '/' + edge.node.data.slug,
+                guid: rssMetadata.siteUrl + '/' + edge.node.data.slug,
+                custom_elements: [
+                  {
+                    'content:encoded': edge.node.data.body.childMarkdownRemark.html,
+                  },
+                ],
+              }))
+            },
+            query: `
+            {
+              allAirtable(limit:1000, sort: {fields: [data___date], order: DESC}) {
+                edges {
+                  node {
+                    data {
+                      title
+                      date
+                      slug
+                      body {
+                        childMarkdownRemark {
+                          excerpt
+                          html
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            `,
+            output: "/rss.xml",
+            title: "R note RSS Feed",
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: `UA-130202268-1`,
