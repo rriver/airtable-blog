ab779ce5839cfcec516d9057c86cbecdfd677596
Ryo Watanabe <ryo.watanabe@gmail.com>
2019-01-13 07:32:42 +0900

    Adjust category title

--- src/templates/category.js
+++ src/templates/category.js
@@ -9,8 +9,6 @@ class categoryTemplate extends React.Component {
   render(){
     const cat = this.props.data.category
     const posts = this.props.data.posts
-    
-    const postCount = posts.edges.length
 
     return (
       <Layout location="category" backLink={true}>
@@ -20,7 +18,7 @@ class categoryTemplate extends React.Component {
           description={`「${cat.data.catname}」関連のノート`}
           slug={cat.data.slug}
         />
-        <h1 className={styles.postTitle}>{`Note${postCount > 1 ? `s` : ``} on ${cat.data.catname}`}</h1>
+        <h1 className={styles.postTitle}>{`「${cat.data.catname}」関連のノート`}</h1>
 
         <ul className={indexStyles.postlist}>
           {posts.edges.map(({ node }, index) => (
